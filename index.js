be21f8815438a74823d91543550d127760e8ca14
Kirby Browning <kirby.browning718@gmail.com>
2020-03-15 12:53:21 -0400

    JS fixes to Load More / Show Less Logic; working on Mobile header

--- index.js
+++ index.js
@@ -9,3 +9,39 @@ if(window.scrollY >= 110) {
 }
 
 });
+
+var deviceL = window.matchMedia("(max-width:1199px)");
+var mobileNav = document.getElementById('hamburger-nav');
+var logo = document.getElementById('logo-container');
+var navLinkContainer = document.getElementById('nav-link-container');
+
+if(deviceL.matches) {
+  logo.classList.add('hide');
+}
+
+
+var loadMore = document.getElementById('load-more');
+var recentPost = document.getElementsByClassName('recent-post');
+
+if(deviceL.matches) {
+
+  for(var i = 0; i < recentPost.length; i++) {
+    recentPost[i].classList.add('hide');
+    recentPost[0].classList.remove('hide');
+    recentPost[1].classList.remove('hide');
+  }
+
+loadMore.addEventListener("click", function (e) {
+   loadMore.classList.toggle('show-less');
+  for(var j = 0; j < recentPost.length; j++) {
+recentPost[j].classList.toggle('hide');
+}
+
+if(loadMore.classList.contains('show-less')) {
+  loadMore.innerHTML = "SHOW LESS <i class='fas fa-angle-up'></i>";
+} else {
+  loadMore.innerHTML = "LOAD MORE <i class='fas fa-spinner'>";
+}
+});
+
+}
