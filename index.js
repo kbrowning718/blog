3e1bd62df2292527a91e8614e9960ad8ef03a86d
Kirby Browning <kirby.browning718@gmail.com>
2020-03-29 20:06:03 -0400

    Added JS click functionality for recent post section on Mobile devices

--- index.js
+++ index.js
@@ -22,3 +22,28 @@ if(deviceM.matches) {
   sidebarD.innerHTML =
   "<div class='sidebar-menu-wrapper-desktop'><div class='search-bar-container'><input type='text' id='search' name='search' placeholder='Search'><div class='search-icon-container'><label class='search-icon' for='search'><i class='fas fa-search'></i></label></div></div><div class='recent-post-header'><i class='fas fa-long-arrow-alt-right'></i> <p class='subtitle-headline strong'>Recent Posts</p></div><div class='recent-post'><h4 class='recent-post-h4'>Recent Post Header Goes Here: A Tale of Lorem Ipsum</h4><p>Thoncus dolor purus non enim praesent. Risus at ultrices.</p><div class='read-more-container'><a href='#'>READ MORE</a><i class='fab fa-first-order'></i></div></div><div class='recent-post'><h4 class='recent-post-h4'>Recent Post Header Goes Here: A Tale of Lorem Ipsum</h4><p>Thoncus dolor purus non enim praesent. Risus at ultrices.</p><div class='read-more-container'><a href='#'>READ MORE</a><i class='fab fa-first-order'></i></div></div><div class='recent-post'><h4 class='recent-post-h4'>Recent Post Header Goes Here: A Tale of Lorem Ipsum</h4><p>Thoncus dolor purus non enim praesent. Risus at ultrices.</p><div class='read-more-container'><a href='#'>READ MORE</a><i class='fab fa-first-order'></i></div></div> <div class='see-all-container'><a href='#'>SEE ALL RECENT POSTS  <i class='fas fa-long-arrow-alt-right'></i></a></p></div><div class='email-subscribe-container'><p class='email-subscribe-header'>Are email updates your thing?</p><p>Stay up-to-date on the latest from Company Name by joining our email list below.</p><div class='email-input-container'><input class='email-input' type='text' id='email' name='email' placeholder='Email'><label class='email-subscribe-label' for='email' id='email-submit'>Submit</label></div><a class='tcs' href='#'>Terms & Conditions</a></div><div class='social-container desktop'><i class='fas fa-envelope'></i><i class='fab fa-facebook-f'></i><i class='fab fa-twitter'></i><i class='fab fa-linkedin-in'></i></div></div>"
 }
+
+var loadMore = document.getElementById('load-more');
+var loadIcon = document.getElementById('spinner');
+  loadMore.addEventListener('click', function (e) {
+    loadMore.classList.toggle('show-less');
+
+    var recentPost = document.getElementsByClassName('recent-post');
+
+    for(i = 0; i < recentPost.length; i++) {
+      if(recentPost[i].classList.contains('hide')) {
+        recentPost[i].classList.remove('hide');
+  }
+}
+
+    if(loadMore.classList.contains('show-less')) {
+      loadMore.innerHTML = "LOAD MORE <i class='fas fa-spinner'>";
+
+      recentPost[2].classList.add('hide');
+      recentPost[3].classList.add('hide');
+} else {
+    loadMore.innerHTML = "SHOW LESS <i class='fas fa-angle-up'></i>";
+
+}
+
+      });
